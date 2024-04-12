1c437244ba5bbbb3469ebb16012cd9aa5798608b
Guillermo Saavedra <gsaavedradorante@paypal.com>
2024-04-12 11:32:34 -0600

    Initial Commit

---
+++ Nethflix/NethflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NethflixApp.swift
+//  Nethflix
+//
+//  Created by Saavedra, Guillermo on 12/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NethflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
